4992852d2ab3d2c4509acdfab108620c155e28d1
ds301056 <ds301056@gmail.com>
2024-02-25 22:46:15 -0500

    v0.11

--- Code/api/oanda_api.py
+++ Code/api/oanda_api.py
@@ -1,6 +1,9 @@
 # Importing necessary libraries
 import requests  # Used for making HTTP requests
+import pandas as pd  # Used for data manipulation and analysis
 import constants.defs as defs  # Importing definitions like API keys and URLs
+from dateutil import parser  # Used for parsing dates
+from datetime import datetime as dt # Used for date and time operations
 
 # Defining the OandaApi class to encapsulate OANDA API interactions
 class OandaApi:
@@ -46,7 +49,7 @@ class OandaApi:
         ok, data = self.make_request(url)
 
         # Checking if the request was successful and the key is in the response
-        if ok and data_key in data:
+        if ok == True and data_key in data:
             return data[data_key]  # Return the specific data requested
         else:
             # Print error and return None if unsuccessful
@@ -65,5 +68,59 @@ class OandaApi:
 
 
 
+
+
+    def fetch_candles(self, pair_name, count=10, granularity="H1", #fetch_candles method to get candles data
+                      price="MBA", date_f=None, date_t=None): #pair_name, count, granularity, price, date_f, date_t are the parameters
+        
+
+        url = f"instruments/{pair_name}/candles" #url for the candles endpoint
+        params = dict( #parameters for the request
+            count = count, #count of candles
+            granularity = granularity, #granularity of candles
+            price = "MBA" #price of candles
+        )
+
+        if date_f is not None and date_t is not None: #if date_f and date_t are not None
+            date_format = "%Y-%m-%dT%H:%M:%SZ" #date format
+            params["from"] = dt.strftime(date_f, date_format) #from date in the format specified by date_format 
+            params["to"] = dt.strftime(date_t, date_format) #to date in the format specified by date_format
+        else:
+            params["count"] = count #count of candles
+
+        
+        ok, data = self.make_request(url, params=params) #make request to the url with the parameters
+
+  
+        # Checking if the request was successful and the key is in the response
+        if ok == True and 'candles' in data:
+            return data['candles']  # Return the specific data requested
+        else:
+            # Print error and return None if unsuccessful
+            print("Error: fetching candles",params,  data)
+            return None
+
+    def get_candles_df(self, data):
+        if len(data) == 0:
+            return pd.DataFrame() #use return pd.DataFrame() not .empty
+
+        prices = ['mid', 'bid', 'ask']
+        ohlc = ['o', 'h', 'l', 'c']#keys for looping 
+
+
+        final_data = []
+        for candle in data:
+            if candle['complete'] == False:
+                continue
+            new_dict = {}
+            new_dict['time'] = parser.parse(candle['time'])
+            new_dict['volume'] = candle['volume']
+        
+            for p in prices:
+                for o in ohlc:
+                    new_dict[f"{p}_{o}"]=float(candle[p][o])
+            final_data.append(new_dict)
+        df = pd.DataFrame.from_dict(final_data)
+        return df
 
 
