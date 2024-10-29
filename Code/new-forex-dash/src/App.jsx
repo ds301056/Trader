59d0288f88e10b1c806aaa4e0cc880db8ce56d14
ds301056 <139257740+ds301056@users.noreply.github.com>
2024-10-29 11:13:41 -0400

    v1

---
+++ Code/new-forex-dash/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css' // Ensure this is correctly imported
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
