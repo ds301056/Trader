217cf52a7766dcf499ec854928aeab70e4a2bd6f
ds301056 <139257740+ds301056@users.noreply.github.com>
2024-11-14 10:52:51 -0500

    fixed links on ui for headlines

--- Code/new-forex-dash/src/components/Headline.jsx
+++ Code/new-forex-dash/src/components/Headline.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import PropTypes from 'prop-types' // Import PropTypes for prop validation
 
 function Headline({ data }) {
   return (
-    <div className='headline'>
-      <a href = {data.link} target="_blank" rel="noreferrer">{data.headline}</a>
+    <div className="headline flex justify-center items-center py-2">
+      <a
+        href={data.link}
+        target="_blank"
+        rel="noreferrer"
+        className="text-gray-800 hover:text-blue-600 font-medium text-lg md:text-base transition-transform transform hover:scale-105 duration-200 block truncate "
+      >
+        {data.headline}
+      </a>
     </div>
   )
+}
+
+// Define propTypes for the component
+Headline.propTypes = {
+  data: PropTypes.shape({
+    headline: PropTypes.string.isRequired, // headline is a required string
+    link: PropTypes.string.isRequired, // link is a required string
+  }).isRequired, // data prop itself is required
 }
 
 export default Headline
